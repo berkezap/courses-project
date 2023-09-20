18fe1d6cdf006a3a15f2f54e3c453eef0e1f8fae
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-09-20 13:52:08 +0300

    courses component

--- courses/src/App.jsx
+++ courses/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Courses from "./Courses";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  const fetchCourses = async () => {
+    const response = await axios.get("http://localhost:3000/courses");
+    setCourses(response.data);
+  };
 
+  useEffect(() => {
+    fetchCourses();
+  }, []);
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <Courses courses={courses} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
